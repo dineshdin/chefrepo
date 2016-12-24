bd735f2d3e23b2c0098b8b333d17d41e8bc611f8
dineshdin <root@ip-172-31-19-64.us-west-2.compute.internal>
2016-12-24 12:44:22 +0000

    starter kit

---
+++ chef-starter/chef-repo/cookbooks/praveencb/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: praveencb
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+package"apache2"do
+ action :install
+end
+
